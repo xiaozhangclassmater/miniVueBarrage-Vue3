d37d89a31cbb8e1e4237f37d6fc2a8f389a8dfaf
zhangqy@feelingtech.net <zhangqy@feelingtech.net>
2024-05-07 09:32:11 +0800

    Merge branch 'dev-0.0.3'

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,5 @@
 declare module 'miniVueBarrage' {
   export interface GlobalComponent {
-<<<<<<< HEAD
-    miniVueBarrage: typeof import('./packages/components/minivueBarrage/src/index.vue')
-=======
     miniVueBarrage: typeof import('./packages/components/minivueBarrage/index')
->>>>>>> dev-0.0.3
   }
 }
