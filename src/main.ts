932f771e032c234d32db3037faa22c2922dc9a11
zhangqy@feelingtech.net <zhangqy@feelingtech.net>
2024-05-13 10:30:38 +0800

    feat:弹幕组件全局注册测试

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import 'normalize.css';
 import { createApp } from 'vue';
-// import { miniVueBarrage } from '../dist/minivuebarrage';
-// import '../dist/minivuebarrage/lib/index.css';
+import { miniVueBarrage } from '../dist/minivuebarrage';
+import '../dist/minivuebarrage/lib/index.css';
 import App from './App.vue';
 import router from './router';
 const app = createApp(App)
-// app.use(miniVueBarrage)
+app.use(miniVueBarrage)
 app.use(router)
 app.use(ElementPlus)
 app.mount("#app")
